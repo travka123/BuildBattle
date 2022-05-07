2c42e0ad39c2bb1a421e342d1adfd7e8f8676dc2
Киселёв Андрей <vkusnaya.travka@gmail.com>
2022-05-08 01:56:48 +0300

    verified connection

---
+++ server/Game/GameHub.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace Server.Game;
+
+[Authorize]
+public class GameHub : Hub {
+
+    private enum ClientState { None, Waiting };
+
+    private static readonly Dictionary<string, string> _verifiedConnections = new();
+    private readonly object _verifyLock = new object();
+
+    public override async Task OnConnectedAsync() {
+
+        bool verified = false;
+
+        var login = Context.User!.Identity!.Name!;
+
+        lock (_verifyLock) {
+
+            if (!_verifiedConnections.ContainsKey(login)) {
+
+                _verifiedConnections.Add(login, Context.ConnectionId);
+
+                verified = true;
+            }
+        }
+
+        if (verified) {
+
+            await OnVerifiedConnectedAsync();
+        }
+        else {
+
+            Context.Abort();
+        }
+
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception) {
+
+        bool verified = false;
+
+        var login = Context.User!.Identity!.Name!;
+
+        lock (_verifyLock) {
+
+            verified = _verifiedConnections[login] == Context.ConnectionId;
+        }
+
+        if (verified) {
+
+            await OnVerifiedDisconnectedAsync(exception);
+        }
+
+        if (verified) {
+
+            lock (_verifyLock) {
+
+                _verifiedConnections.Remove(login);
+            }
+        }
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    private async Task OnVerifiedConnectedAsync() {
+    }
+
+    private async Task OnVerifiedDisconnectedAsync(Exception? exception) {
+    }
+}
+
